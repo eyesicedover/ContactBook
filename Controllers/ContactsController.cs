126ff3429f414b0d2b51121db9f7a1fcf8497223
Stephanie Faber <eyesicedover@gmail.com>
2018-02-09 14:00:14 -0800

    LINA: this is a copy of address-book-csharp because it wouldn't use Razor code. This version works and its paths work. See other repo for previous commit history

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ContactBook.Models;
+
+namespace ContactBook.Controllers
+{
+    public class ContactsController : Controller
+    {
+      [HttpGet("/contacts")]
+      public ActionResult Index()
+      {
+        List<Contact> allContacts = Contact.GetAll();
+        return View(allContacts);
+      }
+
+      [HttpPost("/contacts")]
+      public ActionResult Create()
+      {
+          string name = Request.Form["name"];
+          string phone = Request.Form["phone"];
+          string address = Request.Form["address"];
+          Contact newContact = new Contact(name, phone, address);
+          List<Contact> allContacts = Contact.GetAll();
+          return View("Index", allContacts);
+      }
+
+      [HttpGet("/contacts/new")]
+      public ActionResult NewContactForm()
+      {
+          return View();
+      }
+
+      [HttpGet("/contacts/{id}")]
+      public ActionResult Details(int id)
+      {
+          Contact chosenContact = Contact.FindById(id);
+          return View(chosenContact);
+      }
+
+      [HttpPost("/contacts/clear")]
+      public ActionResult Clear()
+      {
+          Contact.ClearAll();
+          List<Contact> allContacts = Contact.GetAll();
+          return View("Index", allContacts);
+      }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+
+namespace ContactBook.Models
+{
+    public class Contact
+    {
+        private string _name;
+        private string _phoneNumber;
+        private string _address;
+        private int _id;
+        private static List<Contact> _instances = new List<Contact> {};
+
+        public Contact (string name, string phoneNumber, string address)
+        {
+            _name = name;
+            _phoneNumber = phoneNumber;
+            _address = address;
+            _instances.Add(this);
+            _id = _instances.Count;
+        }
+
+        public void SetName(string newName)
+        {
+            _name = newName;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetPhoneNumber(string newPhone)
+        {
+            _phoneNumber = newPhone;
+        }
+
+        public string GetPhoneNumber()
+        {
+            return _phoneNumber;
+        }
+
+        public void SetAddress(string newAddress)
+        {
+            _address = newAddress;
+        }
+
+        public string GetAddress()
+        {
+            return _address;
+        }
+
+        public void SetId(int newId)
+        {
+            _id = newId;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public static List<Contact> GetAll()
+        {
+          return _instances;
+        }
+
+        public static Contact FindById(int searchId)
+        {
+            return _instances[searchId-1];
+        }
+
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
+    }
+}
